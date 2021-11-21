68ee68172f69ced669bc01cbd43dfa139c3597ca
dale <stewartdale45@yahoo.com>
2021-11-20 19:28:51 -0500

    Implemented the functionality to create Login Page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Input from '../components/Input';
+import Button from '../components/Button';
+import axios from 'axios';
+import toast from 'react-hot-toast';
 
-const Login = () => {
+const Login = ({ history }) => {
+  //   const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleClick = async (e) => {
+    // console.log('email and password', email, password);
+    try {
+      e.preventDefault();
+      const { data } = await axios.post('http://localhost:8002/api/login', {
+        // name,
+        email,
+        password,
+      });
+      console.log(data);
+
+      if (data.error) {
+        toast.error(data.error);
+      } else {
+        // setName('');
+        setEmail('');
+        setPassword('');
+
+        history.push('/');
+      }
+    } catch (err) {
+      console.log(err);
+      toast.error('Something went wrong. Try again');
+    }
+  };
   return (
     <div className="d-flex justify-content-center" style={{ height: '80vh' }}>
       <div className="container align-items-center d-flex">
         <div className="row col-md-6 offset-md-3 text-center">
           <h1 className="pt-5 fw-bold">Login</h1>
-          <p className="lead pb-4">...</p>
+          <p className="lead pb-4">
+            Access your subscriptions. Anytime. Anywhere.
+          </p>
+
+          <div className="form-group">
+            {/* <Input label="Name" value={name} setValue={setName} /> */}
+            <Input
+              label="Email"
+              type="email"
+              value={email}
+              setValue={setEmail}
+            />
+            <Input
+              label="Password"
+              type="password"
+              value={password}
+              setValue={setPassword}
+            />
+
+            <div className="d-grid">
+              <Button
+                handleClick={handleClick}
+                type="danger"
+                text="Login"
+                size="sm"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
