b7e76effdc04d62db7f38fd563e42e91f737f468
dale <stewartdale45@yahoo.com>
2021-11-22 17:47:41 -0500

    Implement dynamic pricing cards

--- src/components/cards/PriceCard.js
+++ src/components/cards/PriceCard.js
@@ -1,27 +1,58 @@
-import React from "react";
+import React from 'react';
 
 const PriceCard = ({ price }) => {
+  const dynamicDescription = () => {
+    if (price.nickname === 'BASIC') {
+      return '5 exclusive stocks';
+    } else if (price.nickname === 'PRO') {
+      return '10 exclusive stocks';
+    } else if (price.nickname === 'ULTIMATE') {
+      return '50 exclusive stocks';
+    }
+  };
+  const buttonStyle = () => {
+    return price.nickname === 'BASIC' ? 'btn-outline-danger' : 'btn-danger';
+  };
+
+  const handleSubscription = (price) => {
+    //
+  };
+
+  const headerStyle = () => {
+    return price.nickname === 'PRO' ? 'bg-danger text-light' : '';
+  }
+
+  const borderStyle = () => {
+    return price.nickname === 'PRO' ? 'border-danger' : '';
+  }
   return (
     <div className="col">
-      <div className="card mb-4 rounded-3 shadow-sm">
-        <div className="card-header py-3">
-          <h4 className="my-0 fw-normal">BASIC</h4>
+      <div className={`card mb-4 rounded-3 shadow-sm ${borderStyle()}`}>
+        <div className={`card-header py-3 ${headerStyle()}`}>
+          <h4 className="my-0 fw-normal">{price.nickname}</h4>
         </div>
 
         <div className="card-body">
           <h1 className="card-title pricing-card-title">
-            $50<small className="text-muted fw-light">/mo</small>
+            {(price.unit_amount / 100).toLocaleString('en-US', {
+              style: 'currency',
+              currency: 'USD',
+            })}
+            <small className="text-muted fw-light">/mo</small>
           </h1>
           <ul className="list-unstyled mt-3 mb-4">
-            <li>5 exclusive stocks</li>
+            <li className="fw-bold">{dynamicDescription()}</li>
             <li>Free market analysis</li>
             <li>Email support</li>
             <li>Help center access</li>
           </ul>
 
-          <pre>{JSON.stringify(price, null, 4)}</pre>
+          {/* <pre>{JSON.stringify(price, null, 4)}</pre> */}
 
-          <button className="w-100 btn btn-lg btn-outline-danger">
+          <button
+            onClick={() => handleSubscription(price)}
+            className={`w-100 btn btn-lg ${buttonStyle()}`}
+          >
             Sign up
           </button>
         </div>
