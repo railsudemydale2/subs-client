abcb76792c4642c2222182ffb90378b9d489baf2
dale <stewartdale45@yahoo.com>
2021-11-23 07:56:59 -0500

    Implemented the functionality and logic regarding Subscription status client

--- src/pages/stripe-success.js
+++ src/pages/stripe-success.js
@@ -1,15 +1,28 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import { SyncOutlined } from "@ant-design/icons";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { SyncOutlined } from '@ant-design/icons';
 
-const StripeSuccess = () => {
+const StripeSuccess = ({ history }) => {
+  useEffect(() => {
+    const getSubscriptionStatus = async () => {
+      const { data } = await axios.get('/subscription-status');
+      console.log('SUBSCRIPTION STATUS => ', data);
+      if (data && data.length === 0) {
+        history.push('/');
+      } else {
+        history.push('/account');
+      }
+    };
+
+    getSubscriptionStatus();
+  }, []);
   return (
     <div
       className="d-flex justify-content-center fw-bold"
-      style={{ height: "90vh" }}
+      style={{ height: '90vh' }}
     >
       <div className="d-flex align-items-center">
-        <SyncOutlined spin style={{ fontSize: "50px" }} />
+        <SyncOutlined spin style={{ fontSize: '50px' }} />
       </div>
     </div>
   );
